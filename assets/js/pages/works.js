a79f8ae5a1136cbe7e4fed12c5b9bdf2b5b14139
Kob_asan <kobasato0721@gmail.com>
2026-07-14 21:04:31 +0900

    Create works.js

---
+++ assets/js/pages/works.js
@@ -0,0 +1,13 @@
+/* ==========================================
+   Kob_asan
+   works.js
+========================================== */
+
+
+card.classList.add("is-click");
+
+setTimeout(()=>{
+
+    card.classList.remove("is-click");
+
+},300);
